309d9536ad36e80d9b9a7391b9606ea92cb1e561
josedev-front <130413063+josedev-front@users.noreply.github.com>
2024-06-24 22:12:32 -0400

    se añaden el numero telefonico

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,11 +3,11 @@ const {whatsapp} = require('../lib/whatsapp');
 const router = Router();
 
 router.post('/enviarMensaje', async(req, res)=>{
-  const tel = '+51xxxxxxxxxxxcolocarnúmerotelefonico'
+  const tel = '+56973078318'
   const chatId = tel.substring(1) + "@c.us";
   const number_details = await whatsapp.getNumberId(chatId);
   if(number_details){
-    const mensaje = "Hola, suscribete para más contenido"
+    const mensaje = "Hola, esto es una prueba de JJdevelopers"
     await whatsapp.sendMessage(chatId, mensaje);
     res.json({res: true})
   }else{
